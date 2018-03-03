88e6d7731ccce0bc41a8e919d67daa05c60cb26f
xueqin2017 <532298480@qq.com>
2018-03-03 19:21:11 +0800

    测试提交

---
+++ weixin/src/main/java/com/hxz/weixin/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.hxz.weixin.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String Hello(){
+        return "hello";
+    }
+}
