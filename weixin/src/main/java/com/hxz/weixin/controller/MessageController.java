6d0221498251cf9e1e79c1a773bdcac881d1bfe2
xueqin2017 <532298480@qq.com>
2018-03-04 15:18:13 +0800

    新增：信息回复

---
+++ weixin/src/main/java/com/hxz/weixin/controller/MessageController.java
@@ -0,0 +1,55 @@
+package com.hxz.weixin.controller;
+
+import com.hxz.weixin.utils.MessageUtil;
+import com.hxz.weixin.utils.TextMessageUtil;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+
+/**
+ *
+ * 类名称: BaseMessage
+ * 类描述: 回复消息接口
+ * @author xueqin
+ * 创建时间:2018年3月4日
+ */
+@RestController
+@RequestMapping("/messages")
+public class MessageController {
+
+    @RequestMapping(method= RequestMethod.POST)
+    public void dopost(HttpServletRequest request, HttpServletResponse response){
+        response.setCharacterEncoding("utf-8");
+        PrintWriter out = null;
+        //将微信请求xml转为map格式，获取所需的参数
+        Map<String,String> map = MessageUtil.xmlToMap(request);
+        String ToUserName = map.get("ToUserName");
+        String FromUserName = map.get("FromUserName");
+        String MsgType = map.get("MsgType");
+        String Content = map.get("Content");
+
+        String message = null;
+        //处理文本类型，实现输入1，回复相应的封装的内容
+        if("text".equals(MsgType)){
+            if("1".equals(Content)){
+                TextMessageUtil textMessage = new TextMessageUtil();
+                message = textMessage.initMessage(FromUserName, ToUserName);
+            }
+        }
+        try {
+            out = response.getWriter();
+            out.write(message);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        out.close();
+    }
+}
---
+++ weixin/src/main/java/com/hxz/weixin/utils/TextMessageUtil.java
@@ -0,0 +1,37 @@
+package com.hxz.weixin.utils;
+
+import com.hxz.weixin.dto.MessageText;
+import com.thoughtworks.xstream.XStream;
+
+import java.util.Date;
+
+/**
+ * 类名称: MessageTest
+ * 类描述: 消息内容实体
+ * @author xueqin
+ * 创建时间:20178年3月4日
+ */
+public class TextMessageUtil{
+    /**
+     * 将发送消息封装成对应的xml格式
+     */
+    public  String messageToxml(MessageText message) {
+        XStream xstream  = new XStream();
+        xstream.alias("xml", message.getClass());
+        return xstream.toXML(message);
+    }
+    /**
+     * 封装发送消息对象,封装时，需要将调换发送者和接收者的关系
+     * @param FromUserName
+     * @param ToUserName
+     */
+    public  String initMessage(String FromUserName, String ToUserName) {
+        MessageText text = new MessageText();
+        text.setToUserName(FromUserName);
+        text.setFromUserName(ToUserName);
+        text.setContent("欢迎关注机械振动工程党支部");
+        text.setCreateTime(new Date().getTime());
+        text.setMsgType("text");
+        return  messageToxml(text);
+    }
+}
