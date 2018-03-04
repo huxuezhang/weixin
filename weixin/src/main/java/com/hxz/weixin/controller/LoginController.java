f212b5374e5cc8c324134602bc8a3a4de5c11677
xueqin2017 <532298480@qq.com>
2018-03-04 13:21:30 +0800

    新增：微信登录验证

---
+++ weixin/src/main/java/com/hxz/weixin/controller/LoginController.java
@@ -0,0 +1,43 @@
+package com.hxz.weixin.controller;
+
+import com.hxz.weixin.utils.CheckUtil;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ * 类名称: LoginController
+ * 类描述: 与微信对接登陆验证
+ * @author xueqin
+ * 创建时间:2018年3月4日
+ */
+@Controller
+public class LoginController {
+    @RequestMapping(value = "wx",method= RequestMethod.GET)
+    public void login(HttpServletRequest request, HttpServletResponse response){
+        System.out.println("success");
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+        PrintWriter out = null;
+        try {
+            out = response.getWriter();
+            if(CheckUtil.checkSignature(signature, timestamp, nonce)){
+                out.write(echostr);
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }finally{
+            out.close();
+        }
+
+    }
+}
