09dcfa1375f68010c45a979491c8295122f8bd6d
zhanglandong <1271160495@qq.com>
2018-03-07 20:33:45 +0800

    weixin:修复产生的bug

--- weixin/src/main/java/com/hxz/weixin/dto/MessageText.java
+++ weixin/src/main/java/com/hxz/weixin/dto/MessageText.java
@@ -11,9 +11,15 @@ import lombok.Data;
  */
 @Data
 public class MessageText extends BaseMessage {
+
     private String Content;//文本消息内容
+
     private String MsgId;//消息id，64位整型
 
+    public MessageText(){
+
+    }
+    
     public MessageText(String toUserName, String fromUserName,
                        long createTime, String msgType, String content, String msgId) {
         super();
@@ -24,5 +30,6 @@ public class MessageText extends BaseMessage {
         Content = content;
         MsgId = msgId;
     }
+
 
 }
