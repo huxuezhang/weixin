01339b5829230ceeba6a2cabcfffc40f40ba4274
xueqin2017 <532298480@qq.com>
2018-03-11 23:03:40 +0800

    新增：常量配置类

---
+++ weixin/src/main/java/com/hxz/weixin/commons/Constants.java
@@ -0,0 +1,26 @@
+package com.hxz.weixin.commons;
+
+/**
+ * 类名称: Constants
+ * 类描述: 常量配置
+ *
+ * @author xueqin
+ * 创建时间:2018年3月11日
+ */
+public class Constants {
+
+    /**
+     * 微信自定义菜单创建接口
+     */
+    public static final String WX_MENU_CREATE = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";
+
+    /**
+     * 微信自定义菜单查询接口
+     */
+    public static final String WX_MENU_GET = "https://api.weixin.qq.com/cgi-bin/menu/get?access_token=ACCESS_TOKEN";
+
+    /**
+     * 微信自定义菜单查询接口
+     */
+    public static final String WX_MENU_DELETE = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN\n";
+}
